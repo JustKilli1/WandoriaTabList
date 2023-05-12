40ec5b747ecc403f5959d582cc8ba0871437d325
JustKilli <justkilli@outlook.de>
2023-05-12 14:57:57 +0200

    WAN-10 getter added

--- src/main/java/net/wandoria/tablist/commands/WandoriaCommand.java
+++ src/main/java/net/wandoria/tablist/commands/WandoriaCommand.java
@@ -4,11 +4,23 @@ import org.bukkit.command.CommandExecutor;
 
 public abstract class WandoriaCommand implements CommandExecutor {
 
-    private final String name, syntax, description;
+    protected final String name, syntax, description;
 
     protected WandoriaCommand(String name, String syntax, String description) {
         this.name = name;
         this.syntax = syntax;
         this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSyntax() {
+        return syntax;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
