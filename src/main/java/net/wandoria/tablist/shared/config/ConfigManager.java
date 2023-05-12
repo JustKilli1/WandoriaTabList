98c0c085e63bab389f71edf078407ca2fc537a4a
JustKilli <justkilli@outlook.de>
2023-05-12 11:26:01 +0200

    WAN-3 Config Manager added

---
+++ src/main/java/net/wandoria/tablist/shared/config/ConfigManager.java
@@ -0,0 +1,50 @@
+package net.wandoria.tablist.shared.config;
+
+import net.wandoria.tablist.shared.logging.ILogger;
+import net.wandoria.tablist.shared.logging.LogLevel;
+import net.wandoria.tablist.shared.logging.loggers.BaseLogger;
+import net.wandoria.tablist.shared.logging.output.ConsolePrinter;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.Plugin;
+
+import java.io.File;
+
+public class ConfigManager {
+
+    private File file;
+    private FileConfiguration config;
+    private ILogger logger;
+
+    public ConfigManager(Plugin plugin, String path) {
+        this(plugin.getDataFolder().getAbsolutePath() + "/" + path);
+    }
+
+    public ConfigManager(String path) {
+        file = new File(path);
+        logger = new BaseLogger("Config", new ConsolePrinter());
+        config = YamlConfiguration.loadConfiguration(file);
+    }
+
+    /**
+     * Saves the Config file
+     * @return true if success
+     * */
+    public boolean save() {
+        try {
+            config.save(file);
+            return true;
+        } catch (Exception ex) {
+            logger.log(LogLevel.ERROR,"Could not save ConfigFile with Path: " + file.getAbsolutePath(), ex);
+            return false;
+        }
+    }
+    public void reload() {
+        config = YamlConfiguration.loadConfiguration(file);
+    }
+    public File getFile() { return file; }
+    public FileConfiguration getConfig() {
+        return config;
+    }
+
+}
