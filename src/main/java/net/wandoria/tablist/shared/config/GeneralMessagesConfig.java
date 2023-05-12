6f663554d65bd5972d1dfdf357933a0fb571c469
JustKilli <justkilli@outlook.de>
2023-05-12 11:37:14 +0200

    WAN-3 General Message Config added

---
+++ src/main/java/net/wandoria/tablist/shared/config/GeneralMessagesConfig.java
@@ -0,0 +1,32 @@
+package net.wandoria.tablist.shared.config;
+
+import net.wandoria.tablist.shared.logging.LogLevel;
+
+import java.lang.reflect.Field;
+
+public class GeneralMessagesConfig extends ConfigManager{
+
+    public static final ConfigValue COMMAND_NOT_FOUND = new ConfigValue("general.command_not_found", "Invalid Command Syntax use " + ConfigParams.COMMAND, ConfigParams.COMMAND);
+    public static final ConfigValue ONLY_PLAYER_CAN_EXECUTE = new ConfigValue("general.only_player_can_execute", "Only Player can execute this command");
+    private static final GeneralMessagesConfig instance = new GeneralMessagesConfig();
+
+    private GeneralMessagesConfig() {
+        super("general_messages");
+        createDefaultConfig();
+    }
+
+    private void createDefaultConfig() {
+        for(Field declaredField : this.getClass().getDeclaredFields()) {
+            try {
+                Object value = declaredField.get(null);
+                if(!(value instanceof ConfigValue)) continue;
+                ConfigValue configValue = (ConfigValue) value;
+                addDefault(configValue);
+            } catch(Exception ex) {
+                logger.log(LogLevel.ERROR, "Could not create default Config", ex);
+            }
+        }
+    }
+
+    public static GeneralMessagesConfig getInstance() { return instance; }
+}
