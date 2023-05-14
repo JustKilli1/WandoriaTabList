89dcda74fe4a0e070c039e34664cd93b8957a585
JustKilli <justkilli@outlook.de>
2023-05-14 15:13:50 +0200

    WAN-12 all groups get loaded from Config

--- src/main/java/net/wandoria/tablist/shared/config/TabListConfig.java
+++ src/main/java/net/wandoria/tablist/shared/config/TabListConfig.java
@@ -2,6 +2,8 @@ package net.wandoria.tablist.shared.config;
 
 import net.luckperms.api.LuckPerms;
 import net.luckperms.api.model.group.Group;
+import net.wandoria.tablist.shared.Utils;
+import net.wandoria.tablist.tablist.ViewableGroup;
 import org.bukkit.plugin.Plugin;
 
 import java.util.ArrayList;
@@ -9,10 +11,11 @@ import java.util.List;
 import java.util.Set;
 
 public class TabListConfig extends ConfigManager{
+    private static final String groupRootPath = "groups.";
     private LuckPerms luckPerms;
 
     public TabListConfig(Plugin plugin, LuckPerms luckPerms) {
-        super(plugin, "groups.yml");
+        super(plugin, "tab_list.yml");
         this.luckPerms = luckPerms;
         createDefaultConfig();
     }
@@ -25,11 +28,28 @@ public class TabListConfig extends ConfigManager{
 
         Set<Group> permGroups = luckPerms.getGroupManager().getLoadedGroups();
         for(Group permGroup : permGroups) {
-            String groupPath = "groups." + permGroup.getName();
+            String groupPath = groupRootPath + permGroup.getName();
+            addDefault(new ConfigValue(groupPath + ".name", permGroup.getName()));
+            addDefault(new ConfigValue(groupPath + ".order", ""));
             addDefault(new ConfigValue(groupPath + ".show_in_tab", "false"));
             addDefault(new ConfigValue(groupPath + ".prefix", ""));
             addDefault(new ConfigValue(groupPath + ".suffix", ""));
         }
+    }
+
+    public List<ViewableGroup> getGroups() {
+        List<ViewableGroup> result = new ArrayList<>();
+        Set<Group> permGroups = luckPerms.getGroupManager().getLoadedGroups();
+        for(Group permGroup : permGroups) {
+            String groupPath = groupRootPath + permGroup.getName();
+            String name = getValue(groupPath + ".name");
+            int order = Utils.getInteger(getValue(groupPath + ".order")).orElse(0);
+            boolean showInTab = Utils.getBoolean(getValue(groupPath + ".show_in_tab")).orElse(false);
+            String prefix = getValue(groupPath + ".prefix");
+            String suffix = getValue(groupPath + ".suffix");
+            result.add(new ViewableGroup(name, order, showInTab, prefix, suffix));
+        }
+        return result;
     }
 
 }
